59afd691d13836651cfb7dcafb8ddc971f9225a5
musta <musta@DESKTOP-MUSTAFA>
2024-05-01 09:56:51 +0300

    Add project files.

---
+++ HomeWork2/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+namespace HomeWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Do you want to caluclate using the simple caluclater:");
+            string reaction = Console.ReadLine().ToLower();
+
+            while (reaction == "yes")
+            {
+                Console.WriteLine("Enter the first number");
+                float number1 = Convert.ToSingle(Console.ReadLine());
+
+                Console.WriteLine("Enter the seconed number");
+                float number2 = Convert.ToSingle(Console.ReadLine());
+
+                Console.WriteLine("Choose operation");
+                char operation = Convert.ToChar(Console.ReadLine());
+
+                float summation = 0;
+
+                switch (operation)
+                {
+                    case '+':
+                        summation = number1 + number2;
+                        break;
+                    case '-':
+                        summation = number1 - number2;
+                        break;
+                    case '*':
+                        summation = number1 * number2;
+                        break;
+                    case '/':
+                        summation = number1 / number2;
+                        break;
+                    default:
+                        Console.WriteLine("indecleared operation");
+                        break;
+                }
+                Console.WriteLine($"The summation of the operation is {summation}");
+
+                Console.WriteLine("Do you want to continue?(yes/No ");
+                string calucalteagin = Console.ReadLine();
+                if (calucalteagin == "No")
+                {
+                    Console.WriteLine("Thank you ");
+                    break;
+
+                }
+            }
+            Console.ReadKey();
+
+        }
+    }
+
+}
